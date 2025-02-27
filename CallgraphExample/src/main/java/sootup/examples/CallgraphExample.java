a639ee9ad5a6715850cbfe53db336ac5fa06723e
Rushil Venkateswar <rushilv14@gmail.com>
2025-02-27 17:32:32 +0530

    Add recursive print method to class

--- CallgraphExample/src/main/java/sootup/examples/CallgraphExample.java
+++ CallgraphExample/src/main/java/sootup/examples/CallgraphExample.java
@@ -3,6 +3,7 @@ package sootup.examples;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.HashSet;
 
 import qilin.CoreConfig;
 import qilin.core.PTA;
@@ -17,12 +18,27 @@ import sootup.core.signatures.MethodSignature;
 import sootup.core.typehierarchy.ViewTypeHierarchy;
 import sootup.core.types.ClassType;
 import sootup.core.types.PrimitiveType;
-//import sootup.core.types.VoidType;
+
 import sootup.java.bytecode.frontend.inputlocation.JavaClassPathAnalysisInputLocation;
 import sootup.java.core.JavaIdentifierFactory;
 import sootup.java.core.views.JavaView;
 
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.Level;
+
 public class CallgraphExample {
+  static {
+    // Set logger level programmatically
+    Logger rootLogger = (Logger) LoggerFactory.getLogger("sootup");
+    rootLogger.setLevel(Level.ERROR);
+
+    Logger callgraphLogger = (Logger) LoggerFactory.getLogger("sootup.callgraph");
+    callgraphLogger.setLevel(Level.ERROR);
+
+    Logger typeHierarchyLogger = (Logger) LoggerFactory.getLogger("sootup.core.typehierarchy");
+    typeHierarchyLogger.setLevel(Level.ERROR);
+  }
 
   public static void main(String[] args) {
 //    CoreConfig config = new CoreConfig();
@@ -59,13 +75,15 @@ public class CallgraphExample {
     System.out.println("-------------CHA---------------");
     CallGraphAlgorithm cha = new ClassHierarchyAnalysisAlgorithm(view);
     CallGraph cg1 = cha.initialize(Collections.singletonList(entryMethodSignature));
-    cg1.callsFrom(entryMethodSignature).forEach(System.out::println);
+//    cg1.callsFrom(entryMethodSignature).forEach(System.out::println);
+    recursivePrint(entryMethodSignature, cg1, new HashSet<>(), "");
 
     // Create CG by using RTA with entry method(s)
     System.out.println("-------------RTA---------------");
     CallGraphAlgorithm rta = new RapidTypeAnalysisAlgorithm(view);
     CallGraph cg2 = rta.initialize(Collections.singletonList(entryMethodSignature));
-    cg2.callsFrom(entryMethodSignature).forEach(System.out::println);
+//    cg2.callsFrom(entryMethodSignature).forEach(System.out::println);
+    recursivePrint(entryMethodSignature, cg2, new HashSet<>(), "");
 
 //    // Create CG by using QPA with entry method(s)
 //    System.out.println("-------------PTA---------------");
@@ -75,4 +93,19 @@ public class CallgraphExample {
 //    CallGraph cg3 = pta.getCallGraph();
 //    cg3.callsFrom(entryMethodSignature).forEach(System.out::println);
   }
+
+  public static void recursivePrint(MethodSignature m, CallGraph cg, HashSet<Integer> visited, String indent) {
+      // Base case: if the method has already been visited, return to avoid infinite recursion
+      if (visited.contains(m.hashCode())) {
+        return;
+      }
+      // Pretty print the node
+      System.out.println(indent + " -> " + m);
+
+      // Mark the method as visited
+      visited.add(m.hashCode());
+
+      // Pretty print the children with indentation
+      cg.callTargetsFrom(m).forEach(child -> recursivePrint(child, cg, visited, indent + "    "));
+    }
 }
