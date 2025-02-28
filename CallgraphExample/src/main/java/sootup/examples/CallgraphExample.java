0d544131fdb7f7eda076de4257b5edfe66de7de9
Rushil Venkateswar <rushilv14@gmail.com>
2025-02-28 22:04:00 +0530

    Add TEAMMATES/teammates example .jar and generate its call graph

--- CallgraphExample/src/main/java/sootup/examples/CallgraphExample.java
+++ CallgraphExample/src/main/java/sootup/examples/CallgraphExample.java
@@ -5,21 +5,20 @@ import java.util.Collections;
 import java.util.List;
 import java.util.HashSet;
 
-import qilin.CoreConfig;
-import qilin.core.PTA;
-import qilin.driver.PTAFactory;
-import qilin.driver.PTAPattern;
 import sootup.callgraph.CallGraph;
 import sootup.callgraph.CallGraphAlgorithm;
 import sootup.callgraph.ClassHierarchyAnalysisAlgorithm;
 import sootup.callgraph.RapidTypeAnalysisAlgorithm;
 import sootup.core.inputlocation.AnalysisInputLocation;
+import sootup.core.model.SourceType;
 import sootup.core.signatures.MethodSignature;
 import sootup.core.typehierarchy.ViewTypeHierarchy;
 import sootup.core.types.ClassType;
 import sootup.core.types.PrimitiveType;
-
+import sootup.core.types.VoidType;
+import sootup.core.types.ArrayType;
 import sootup.java.bytecode.frontend.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.java.bytecode.frontend.inputlocation.JrtFileSystemAnalysisInputLocation;
 import sootup.java.core.JavaIdentifierFactory;
 import sootup.java.core.views.JavaView;
 
@@ -41,34 +40,41 @@ public class CallgraphExample {
   }
 
   public static void main(String[] args) {
-//    CoreConfig config = new CoreConfig();
-//    System.out.println(config.getPtaConfig());
-    // Create a AnalysisInputLocation, which points to a directory. All class files will be loaded
-    // from the directory
     List<AnalysisInputLocation> inputLocations = new ArrayList<>();
     inputLocations.add(
-            new JavaClassPathAnalysisInputLocation("src/test/resources/Callgraph/binary"));
+        new JavaClassPathAnalysisInputLocation("src/test/resources/teammates_package/teammates.jar")
+    );
+//    inputLocations.add(
+//        new JrtFileSystemAnalysisInputLocation()
+//    );
 
     JavaView view = new JavaView(inputLocations);
+//    view.getClasses().forEach(System.out::println);
 
-    // Get a MethodSignature
-    ClassType classTypeA = view.getIdentifierFactory().getClassType("A");
-    ClassType classTypeC = view.getIdentifierFactory().getClassType("C");
-    MethodSignature entryMethodSignature =
-            JavaIdentifierFactory.getInstance()
-                    .getMethodSignature(
-                            classTypeC,
-                            JavaIdentifierFactory.getInstance()
-                                    .getMethodSubSignature(
-                                        "calc",
-                                        PrimitiveType.IntType.getInstance(),
-                                        Collections.singletonList(PrimitiveType.IntType.getInstance())
-                                    )
-                    );
+//    ClassType mainType = view.getIdentifierFactory().getClassType("teammates.main.Application");
+//    ClassType StringType = view.getIdentifierFactory().getClassType("java.lang.String");
+//    MethodSignature entryMethodSignature = JavaIdentifierFactory.getInstance().getMethodSignature(
+//        mainType,
+//        JavaIdentifierFactory.getInstance().getMethodSubSignature(
+//            "main",
+//            VoidType.getInstance(),
+//            Collections.singletonList(ArrayType.createArrayType(StringType, 1))
+//        )
+//    );
+
+    ClassType createAccountRequestActionType = view.getIdentifierFactory().getClassType("teammates.ui.webapi.CreateAccountRequestAction");
+    MethodSignature entryMethodSignature = JavaIdentifierFactory.getInstance().getMethodSignature(
+        createAccountRequestActionType,
+        JavaIdentifierFactory.getInstance().getMethodSubSignature(
+            "execute",
+            view.getIdentifierFactory().getClassType("teammates.ui.webapi.JsonResult"),
+            Collections.emptyList()
+        )
+    );
 
 //    // Create type hierarchy and CHA
 //    final ViewTypeHierarchy typeHierarchy = new ViewTypeHierarchy(view);
-//    typeHierarchy.subclassesOf(classTypeA).forEach(System.out::println);
+//    typeHierarchy.subclassesOf(testClassType).forEach(System.out::println);
 
 
     // Create CG by initializing CHA with entry method(s)
@@ -78,12 +84,12 @@ public class CallgraphExample {
 //    cg1.callsFrom(entryMethodSignature).forEach(System.out::println);
     recursivePrint(entryMethodSignature, cg1, new HashSet<>(), "");
 
-    // Create CG by using RTA with entry method(s)
-    System.out.println("-------------RTA---------------");
-    CallGraphAlgorithm rta = new RapidTypeAnalysisAlgorithm(view);
-    CallGraph cg2 = rta.initialize(Collections.singletonList(entryMethodSignature));
-//    cg2.callsFrom(entryMethodSignature).forEach(System.out::println);
-    recursivePrint(entryMethodSignature, cg2, new HashSet<>(), "");
+//    // Create CG by using RTA with entry method(s)
+//    System.out.println("-------------RTA---------------");
+//    CallGraphAlgorithm rta = new RapidTypeAnalysisAlgorithm(view);
+//    CallGraph cg2 = rta.initialize(Collections.singletonList(entryMethodSignature));
+////    cg2.callsFrom(entryMethodSignature).forEach(System.out::println);
+//    recursivePrint(entryMethodSignature, cg2, new HashSet<>(), "");
 
 //    // Create CG by using QPA with entry method(s)
 //    System.out.println("-------------PTA---------------");
@@ -107,5 +113,5 @@ public class CallgraphExample {
 
       // Pretty print the children with indentation
       cg.callTargetsFrom(m).forEach(child -> recursivePrint(child, cg, visited, indent + "    "));
-    }
+  }
 }
